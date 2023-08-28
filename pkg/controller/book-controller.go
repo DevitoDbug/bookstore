42e2c51cbf9cccb9cd41f32bcae770d3a821aec6
DevitoDbug <Davidochiengy@gmail.com>
2023-08-28 14:16:26 +0300

    Defining the getBooks handler

--- pkg/controller/book-controller.go
+++ pkg/controller/book-controller.go
@@ -1,13 +1,30 @@
 package controller
 
-import "net/http"
+import (
+	"encoding/json"
+	"github.com/DevitoDbug/bookstore/pkg/models"
+	"log"
+	"net/http"
+)
+
+var NewBook models.Book
 
 func GetBookById(w http.ResponseWriter, r *http.Request) {
-	
+	//get the id
+	//using params
+	//get the book form the database
 }
 
 func GetBooks(w http.ResponseWriter, r *http.Request) {
-
+	newBooks := models.GetAllBooks()
+	res, _ := json.Marshal(newBooks)
+	w.Header().Set("Content-Type", "pkg/json")
+	w.WriteHeader(http.StatusOK)
+	_, err := w.Write(res)
+	if err != nil {
+		log.Printf("%v\n", err)
+		return
+	}
 }
 
 func CreateBook(w http.ResponseWriter, r *http.Request) {
