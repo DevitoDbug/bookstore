ec7139b355dc5cade09c98a21d23669d1e45adae
DevitoDbug <Davidochiengy@gmail.com>
2023-08-28 14:37:11 +0300

    Defining the getBookById handler

--- pkg/controller/book-controller.go
+++ pkg/controller/book-controller.go
@@ -2,23 +2,46 @@ package controller
 
 import (
 	"encoding/json"
+	"fmt"
 	"github.com/DevitoDbug/bookstore/pkg/models"
+	"github.com/gorilla/mux"
 	"log"
 	"net/http"
+	"strconv"
 )
 
 var NewBook models.Book
 
 func GetBookById(w http.ResponseWriter, r *http.Request) {
-	//get the id
-	//using params
-	//get the book form the database
+	params := mux.Vars(r)
+	id, err := strconv.ParseInt(params["id"], 0, 0)
+	if err != nil {
+		fmt.Println("Error while parsing")
+		return
+	}
+
+	book, dbOperation := models.GetBookById(id)
+	if dbOperation.Error != nil {
+		log.Printf("DB: %v\n", dbOperation.Error)
+		return
+	}
+	res, _ := json.Marshal(book)
+
+	w.Header().Set("Content-Type", "pkglication/json")
+	w.WriteHeader(http.StatusOK)
+
+	_, err = w.Write(res)
+	if err != nil {
+		log.Printf("%v\n", err)
+		return
+	}
+
 }
 
 func GetBooks(w http.ResponseWriter, r *http.Request) {
 	newBooks := models.GetAllBooks()
 	res, _ := json.Marshal(newBooks)
-	w.Header().Set("Content-Type", "pkg/json")
+	w.Header().Set("Content-Type", "pkglication/json")
 	w.WriteHeader(http.StatusOK)
 	_, err := w.Write(res)
 	if err != nil {
